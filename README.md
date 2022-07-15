33546ebd9103a0897928a1d28bbee7092fa3bccb
IkeVoodoo <simone.coletti07@gmail.com>
2022-07-15 09:55:44 +0200

    Added UserError.java and README.md

---
+++ README.md
@@ -0,0 +1,48 @@
+## Juerr
+`Juerr` is a Java port of the [uerr](https://crates.io/crates/uerr) crate, it provides stunning visual error handling.
+
+# Showcase
+Using the code below, we can display a simple error and show it to the user.
+
+```java
+
+import me.ikevoodoo.UserError;
+
+public class ErrorTest {
+    public static void main(String[] args) {
+        UserError.from("could not open file")
+                .addReason("The system cannot find the file specified.")
+                .addHelp("Does this file exist?")
+                .printAll("juerr/error: ");
+    }
+}
+```
+### Output
+```
+juerr/error: could not open file
+- caused by: The system cannot find the file specified.
++ help: Does this file exist?
+```
+## WIth multiple arguments
+```java
+import me.ikevoodoo.UserError;
+
+public class ErrorTest {
+    public static void main(String[] args) {
+        UserError.from("could not open file")
+                .addReason("The system cannot find the file specified.")
+                .addReason("Fillter reason")
+                .addHelp("Does this file exist?")
+                .addHelp("Filler help.")
+                .printAll("program.jar: ");
+    }
+}
+```
+### Output
+```
+program.jar: could not open file
+ - caused by: The system cannot find the file specified.
+     |        Filler reason.
+ + help: Does this file exist?
+     |   Filler help.
+```
---
+++ src/main/java/me/ikevoodoo/UserError.java
@@ -0,0 +1,75 @@
+package me.ikevoodoo;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unused")
+public class UserError {
+
+    private final String message;
+    private final List<String> help;
+    private final List<String> reasons;
+
+    public UserError(String message) {
+        this.message = message;
+        this.help = new ArrayList<>();
+        this.reasons = new ArrayList<>();
+    }
+
+    public static UserError from(String message) {
+        return new UserError(message);
+    }
+
+    public static UserError intoUserError(Error error) {
+        return new UserError(error.getLocalizedMessage());
+    }
+
+    public static UserError intoUserError(Exception exception) {
+        return new UserError(exception.getLocalizedMessage());
+    }
+
+    public UserError printAll(String prefix) {
+        return this.printAll(System.err, prefix);
+    }
+
+    public UserError printAll(PrintStream stream, String prefix) {
+        this.print(stream, prefix);
+        return this;
+    }
+
+    public UserError addHelp(String help) {
+        this.help.add(help);
+        return this;
+    }
+
+    public UserError addReason(String reason) {
+        this.reasons.add(reason);
+        return this;
+    }
+
+    public String message() {
+        return this.message;
+    }
+
+    public List<String> reasons() {
+        return this.reasons;
+    }
+
+    public List<String> help() {
+        return this.help;
+    }
+
+    private void print(PrintStream stream, String prefix) {
+        stream.printf("%s%s\n", prefix, this.message);
+        this.printList(stream, this.reasons, " - caused by: ", "     |        ");
+        this.printList(stream, this.help, " + help: ", !this.reasons.isEmpty() ? "     |        " : "     |   ");
+    }
+
+    private void printList(PrintStream stream, List<String> list, String prefix, String joiner) {
+        if (list.isEmpty()) return;
+        stream.printf("%s%s\n", prefix, list.get(0));
+        list.subList(1, list.size()).forEach(entry -> stream.printf("%s%s\n", joiner, entry));
+    }
+
+}
